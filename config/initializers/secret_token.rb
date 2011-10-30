036caedf190bcbd0aa3c31faae9d312f406189f1
Yoshihide Chubachi <yoshi@chubachi.net>
2011-10-30 17:40:43 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3OpenidExample::Application.config.secret_token = '1ebf69dd9b86dbcffac6505edd99519f09969d726a797697d3a03d892e11b5c67daba44992f22358aa9db78cbc47553cc2de8fca92133a9c1a4bd47b4c004be2'

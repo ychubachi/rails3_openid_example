10e80c6f9f34429013843f3861b4db7b91d8707c
Yoshihide Chubachi <yoshi@chubachi.net>
2011-10-30 17:47:08 +0900

    generated consumer controller

---
+++ app/controllers/consumer_controller.rb
@@ -0,0 +1,5 @@
+class ConsumerController < ApplicationController
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails3OpenidExample::Application.routes.draw do
+  get "consumer/index"
+  root :to => 'consumer#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
